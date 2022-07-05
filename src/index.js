8c29885fc90b3f6a5c70e54e57045db8314360b3
Kirill Komaryshkin <106930216+KirillEvg@users.noreply.github.com>
2022-07-05 17:27:17 +0300

    fix: update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function reverse (n) {
             let a = ((n.toString()).replace(/-/g, '')).split('') ;
-       alert((a.reverse().join('')));
+       alert(a.reverse().join(''));
 }
